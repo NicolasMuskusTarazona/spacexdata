6707009b59cc43ee0055f7f288204d357e3ce8a3
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-03-31 09:08:52 -0500

    feat: :bug: `Print api in console`
    main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let getALL = async() => {
     const url = "https://api.spacexdata.com/v3/rockets"
     const config = {
-        METHOD: "GET"
+        method: "GET",
     }
-    const response = await fetch(url, config)
+    const response = await fetch(url,config)
     return response.json()
 }
+console.log(await getALL())
