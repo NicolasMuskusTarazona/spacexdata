591a3f1372c6e5d1ed3276ef0bd62baed9780845
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-03-31 22:34:36 -0500

    feat: :sparkles: `Descripcion corta.`
    2. Obtener una description corta usando substring delimitado por puntos

--- main.js
+++ main.js
@@ -16,12 +16,17 @@ let filtrarTipos = async() => {
     const response = await fetch(url,config)
     let data =await response.json()
     let filtrar = data.filter(tipoMerlin =>{
-        if(tipoMerlin.engines.type  === "merlin")
-        console.log(`Numero motor ${tipoMerlin.engines.number} Tipo: ${tipoMerlin.engines.type} y Nombre: ${tipoMerlin.rocket_name}`)
+        if(tipoMerlin.engines.type  === "merlin"){
+            console.log(`Numero motor: ${tipoMerlin.engines.number} Tipo: ${tipoMerlin.engines.type} y Nombre: ${tipoMerlin.rocket_name}`)
+            tipoMerlin.engines.number == 1 && console.log(`Descripcion: ${tipoMerlin.description.substring(0, 110)}` )
+            tipoMerlin.engines.number == 9 && console.log(`Descripcion: ${tipoMerlin.description.substring(0, 160)}` )
+            tipoMerlin.engines.number == 27 && console.log(`Descripcion: ${tipoMerlin.description.substring(0, 300)}` )
+        }
     })
     let filtrar2 = data.filter(filtrarRaptor =>{
         if(filtrarRaptor.engines.type === "raptor")
         console.log(`Numero motor: ${filtrarRaptor.engines.number} Tipo: ${filtrarRaptor.engines.type} y Nombre: ${filtrarRaptor.rocket_name}`)
+        filtrarRaptor.engines.number == 37 && console.log( `Descripcion: ${filtrarRaptor.description.substring(0, 152)}`)
     })
     return filtrar, filtrar2
 }
