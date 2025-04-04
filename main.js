f9815baf32b6ec784a10c375e495e043860889b2
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-04-04 16:36:52 -0500

    feat: :sparkles: `Total costo por lanzamiento`
    3. Obtener solo los `name` de `payload_weights`

--- main.js
+++ main.js
@@ -1,45 +1,47 @@
-let getALL = async() => {
+// Obtener toda la api
+let getALL = async () => {
     const url = "https://api.spacexdata.com/v3/rockets"
     const config = {
         method: "GET",
     }
-    const response = await fetch(url,config)
+    const response = await fetch(url, config)
     return response.json()
 }
 console.log(await getALL())
-
-let filtrarTipos = async() => {
+// 1. Filtrar los `engines` de tipo *merlin* y *raptor* y obtener `rocket_name`
+// 2. Obtener una `description` corta usando `substring` delimitado por puntos
+let filtrarTipos = async () => {
     const url = "https://api.spacexdata.com/v3/rockets"
     const config = {
         method: "GET",
     }
-    const response = await fetch(url,config)
-    let data =await response.json()
-    let filtrar = data.filter(tipoMerlin =>{
-        if(tipoMerlin.engines.type  === "merlin"){
+    const response = await fetch(url, config)
+    let data = await response.json()
+    let filtrar = data.filter(tipoMerlin => {
+        if (tipoMerlin.engines.type === "merlin") {
             console.log(`Numero motor: ${tipoMerlin.engines.number} Tipo: ${tipoMerlin.engines.type} y Nombre: ${tipoMerlin.rocket_name}`)
-            tipoMerlin.engines.number == 1 && console.log(`Descripcion: ${tipoMerlin.description.substring(0, 110)}` )
-            tipoMerlin.engines.number == 9 && console.log(`Descripcion: ${tipoMerlin.description.substring(0, 160)}` )
-            tipoMerlin.engines.number == 27 && console.log(`Descripcion: ${tipoMerlin.description.substring(0, 300)}` )
+            tipoMerlin.engines.number == 1 && console.log(`Descripcion: ${tipoMerlin.description.substring(0, 110)}`)
+            tipoMerlin.engines.number == 9 && console.log(`Descripcion: ${tipoMerlin.description.substring(0, 160)}`)
+            tipoMerlin.engines.number == 27 && console.log(`Descripcion: ${tipoMerlin.description.substring(0, 300)}`)
         }
     })
-    let filtrar2 = data.filter(filtrarRaptor =>{
-        if(filtrarRaptor.engines.type === "raptor")
-        console.log(`Numero motor: ${filtrarRaptor.engines.number} Tipo: ${filtrarRaptor.engines.type} y Nombre: ${filtrarRaptor.rocket_name}`)
-        filtrarRaptor.engines.number == 37 && console.log( `Descripcion: ${filtrarRaptor.description.substring(0, 152)}`)
+    let filtrar2 = data.filter(filtrarRaptor => {
+        if (filtrarRaptor.engines.type === "raptor")
+            console.log(`Numero motor: ${filtrarRaptor.engines.number} Tipo: ${filtrarRaptor.engines.type} y Nombre: ${filtrarRaptor.rocket_name}`)
+        filtrarRaptor.engines.number == 37 && console.log(`Descripcion: ${filtrarRaptor.description.substring(0, 152)}`)
     })
     return filtrar, filtrar2
 }
 await filtrarTipos()
-
-let weightsNames = async() => {
+// 3. Obtener solo los `name` de `payload_weights`
+let weightsNames = async () => {
     const url = "https://api.spacexdata.com/v3/rockets"
     const config = {
         method: "GET",
     }
-    const response = await fetch(url,config)
-    let data =await response.json()
-    let nombresotes = data.filter(nombres =>{
+    const response = await fetch(url, config)
+    let data = await response.json()
+    let nombresotes = data.filter(nombres => {
         nombres.payload_weights.forEach(nombresitos => {
             console.log(`Pesos de carga util Nombres: ${nombresitos.name}`)
         });
@@ -47,17 +49,17 @@ let weightsNames = async() => {
     return nombresotes
 }
 await weightsNames()
-let costPerLaunch = async() => {
+
+// 4. Calcular el total de los `cost_per_launch` de todos los rockets
+let costPerLaunch = async () => {
     const url = "https://api.spacexdata.com/v3/rockets"
     const config = {
         method: "GET",
     }
-    const response = await fetch(url,config)
-    let data =await response.json()
-    let costoLanzamiento = data.filter(costo =>{
-        let numero1 = costo.id == 1 && console.log(costo.cost_per_launch)
-        let numero2 = costo.id == 2 && console.log(costo.cost_per_launch)
-    })
+    const response = await fetch(url, config)
+    let data = await response.json()
+    let costoLanzamiento = data.reduce((sum,value) => (sum + value.cost_per_launch), 0)
+    console.log(`Total costo por lanzamiento: ${costoLanzamiento} $`)
     return costoLanzamiento
 }
 await costPerLaunch()
